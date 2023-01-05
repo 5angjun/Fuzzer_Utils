75220ae175887f6c1fab640cdc66384198ffbb61
sangjun <76202024+5angjun@users.noreply.github.com>
2023-01-05 21:50:16 +0900

    Create cli.c

---
+++ cli.c
@@ -0,0 +1,383 @@
+/**************************************************************************
+ * simpletun.c                                                            *
+ *                                                                        *
+ * A simplistic, simple-minded, naive tunnelling program using tun/tap    *
+ * interfaces and TCP. DO NOT USE THIS PROGRAM FOR SERIOUS PURPOSES.      *
+ *                                                                        *
+ * You have been warned.                                                  *
+ *                                                                        *
+ * (C) 2010 Davide Brini.                                                 *
+ *                                                                        *
+ * DISCLAIMER AND WARNING: this is all work in progress. The code is      *
+ * ugly, the algorithms are naive, error checking and input validation    *
+ * are very basic, and of course there can be bugs. If that's not enough, *
+ * the program has not been thoroughly tested, so it might even fail at   *
+ * the few simple things it should be supposed to do right.               *
+ * Needless to say, I take no responsibility whatsoever for what the      *
+ * program might do. The program has been written mostly for learning     *
+ * purposes, and can be used in the hope that is useful, but everything   *
+ * is to be taken "as is" and without any kind of warranty, implicit or   *
+ * explicit. See the file LICENSE for further details.                    *
+ *************************************************************************/ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <linux/if_tun.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arpa/inet.h> 
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <stdarg.h>
+
+/* buffer for reading from tun/tap interface, must be >= 1500 */
+#define BUFSIZE 2000   
+#define CLIENT 0
+#define SERVER 1
+#define PORT 55555
+
+int debug;
+char *progname;
+
+/**************************************************************************
+ * tun_alloc: allocates or reconnects to a tun/tap device. The caller     *
+ *            must reserve enough space in *dev.                          *
+ **************************************************************************/
+int tun_alloc(char *dev, int flags) {
+
+  struct ifreq ifr;
+  int fd, err;
+  char *clonedev = "/dev/net/tun";
+
+  if( (fd = open(clonedev , O_RDWR)) < 0 ) {
+    perror("Opening /dev/net/tun");
+    return fd;
+  }
+
+  memset(&ifr, 0, sizeof(ifr));
+
+  ifr.ifr_flags = flags;
+
+  if (*dev) {
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+  }
+
+  if( (err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0 ) {
+    perror("ioctl(TUNSETIFF)");
+    close(fd);
+    return err;
+  }
+
+  strcpy(dev, ifr.ifr_name);
+
+  return fd;
+}
+
+/**************************************************************************
+ * cread: read routine that checks for errors and exits if an error is    *
+ *        returned.                                                       *
+ **************************************************************************/
+int cread(int fd, char *buf, int n){
+  
+  int nread;
+
+  if((nread=read(fd, buf, n)) < 0){
+    perror("Reading data");
+    exit(1);
+  }
+  return nread;
+}
+
+/**************************************************************************
+ * cwrite: write routine that checks for errors and exits if an error is  *
+ *         returned.                                                      *
+ **************************************************************************/
+int cwrite(int fd, char *buf, int n){
+  
+  int nwrite;
+
+  if((nwrite=write(fd, buf, n)) < 0){
+    perror("Writing data");
+    exit(1);
+  }
+  return nwrite;
+}
+
+/**************************************************************************
+ * read_n: ensures we read exactly n bytes, and puts them into "buf".     *
+ *         (unless EOF, of course)                                        *
+ **************************************************************************/
+int read_n(int fd, char *buf, int n) {
+
+  int nread, left = n;
+
+  while(left > 0) {
+    if ((nread = cread(fd, buf, left)) == 0){
+      return 0 ;      
+    }else {
+      left -= nread;
+      buf += nread;
+    }
+  }
+  return n;  
+}
+
+/**************************************************************************
+ * do_debug: prints debugging stuff (doh!)                                *
+ **************************************************************************/
+void do_debug(char *msg, ...){
+  
+  va_list argp;
+  
+  if(debug) {
+	va_start(argp, msg);
+	vfprintf(stderr, msg, argp);
+	va_end(argp);
+  }
+}
+
+/**************************************************************************
+ * my_err: prints custom error messages on stderr.                        *
+ **************************************************************************/
+void my_err(char *msg, ...) {
+
+  va_list argp;
+  
+  va_start(argp, msg);
+  vfprintf(stderr, msg, argp);
+  va_end(argp);
+}
+
+/**************************************************************************
+ * usage: prints usage and exits.                                         *
+ **************************************************************************/
+void usage(void) {
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, "%s -i <ifacename> [-s|-c <serverIP>] [-p <port>] [-u|-a] [-d]\n", progname);
+  fprintf(stderr, "%s -h\n", progname);
+  fprintf(stderr, "\n");
+  fprintf(stderr, "-i <ifacename>: Name of interface to use (mandatory)\n");
+  fprintf(stderr, "-s|-c <serverIP>: run in server mode (-s), or specify server address (-c <serverIP>) (mandatory)\n");
+  fprintf(stderr, "-p <port>: port to listen on (if run in server mode) or to connect to (in client mode), default 55555\n");
+  fprintf(stderr, "-u|-a: use TUN (-u, default) or TAP (-a)\n");
+  fprintf(stderr, "-d: outputs debug information while running\n");
+  fprintf(stderr, "-h: prints this help text\n");
+  exit(1);
+}
+
+int main(int argc, char *argv[]) {
+  
+  int tap_fd, option;
+  int flags = IFF_TUN;
+  char if_name[IFNAMSIZ] = "";
+  int maxfd;
+  uint16_t nread, nwrite, plength;
+  char buffer[BUFSIZE];
+  struct sockaddr_in local, remote;
+  char remote_ip[16] = "";            /* dotted quad IP string */
+  unsigned short int port = PORT;
+  int sock_fd, net_fd, optval = 1;
+  socklen_t remotelen;
+  int cliserv = -1;    /* must be specified on cmd line */
+  unsigned long int tap2net = 0, net2tap = 0;
+
+  progname = argv[0];
+  
+  /* Check command line options */
+  while((option = getopt(argc, argv, "i:sc:p:uahd")) > 0) {
+    switch(option) {
+      case 'd':
+        debug = 1;
+        break;
+      case 'h':
+        usage();
+        break;
+      case 'i':
+        strncpy(if_name,optarg, IFNAMSIZ-1);
+        break;
+      case 's':
+        cliserv = SERVER;
+        break;
+      case 'c':
+        cliserv = CLIENT;
+        strncpy(remote_ip,optarg,15);
+        break;
+      case 'p':
+        port = atoi(optarg);
+        break;
+      case 'u':
+        flags = IFF_TUN;
+        break;
+      case 'a':
+        flags = IFF_TAP;
+        break;
+      default:
+        my_err("Unknown option %c\n", option);
+        usage();
+    }
+  }
+
+  argv += optind;
+  argc -= optind;
+
+  if(argc > 0) {
+    my_err("Too many options!\n");
+    usage();
+  }
+
+  if(*if_name == '\0') {
+    my_err("Must specify interface name!\n");
+    usage();
+  } else if(cliserv < 0) {
+    my_err("Must specify client or server mode!\n");
+    usage();
+  } else if((cliserv == CLIENT)&&(*remote_ip == '\0')) {
+    my_err("Must specify server address!\n");
+    usage();
+  }
+
+  /* initialize tun/tap interface */
+  if ( (tap_fd = tun_alloc(if_name, flags | IFF_NO_PI)) < 0 ) {
+    my_err("Error connecting to tun/tap interface %s!\n", if_name);
+    exit(1);
+  }
+
+  do_debug("Successfully connected to interface %s\n", if_name);
+
+  if ( (sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket()");
+    exit(1);
+  }
+
+  if(cliserv == CLIENT) {
+    /* Client, try to connect to server */
+
+    /* assign the destination address */
+    memset(&remote, 0, sizeof(remote));
+    remote.sin_family = AF_INET;
+    remote.sin_addr.s_addr = inet_addr(remote_ip);
+    remote.sin_port = htons(port);
+
+    /* connection request */
+    if (connect(sock_fd, (struct sockaddr*) &remote, sizeof(remote)) < 0) {
+      perror("connect()");
+      exit(1);
+    }
+
+    net_fd = sock_fd;
+    do_debug("CLIENT: Connected to server %s\n", inet_ntoa(remote.sin_addr));
+    
+  } else {
+    /* Server, wait for connections */
+
+    /* avoid EADDRINUSE error on bind() */
+    if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&optval, sizeof(optval)) < 0) {
+      perror("setsockopt()");
+      exit(1);
+    }
+    
+    memset(&local, 0, sizeof(local));
+    local.sin_family = AF_INET;
+    local.sin_addr.s_addr = htonl(INADDR_ANY);
+    local.sin_port = htons(port);
+    if (bind(sock_fd, (struct sockaddr*) &local, sizeof(local)) < 0) {
+      perror("bind()");
+      exit(1);
+    }
+    
+    if (listen(sock_fd, 5) < 0) {
+      perror("listen()");
+      exit(1);
+    }
+    
+    /* wait for connection request */
+    remotelen = sizeof(remote);
+    memset(&remote, 0, remotelen);
+    if ((net_fd = accept(sock_fd, (struct sockaddr*)&remote, &remotelen)) < 0) {
+      perror("accept()");
+      exit(1);
+    }
+
+    do_debug("SERVER: Client connected from %s\n", inet_ntoa(remote.sin_addr));
+  }
+  system("ifconfig tun70 up");
+  /* use select() to handle two descriptors at once */
+  maxfd = (tap_fd > net_fd)?tap_fd:net_fd;
+  int kread;
+  sleep(5);
+      kread = read(net_fd,buffer,sizeof(buffer));
+    if(kread < 0) {
+      perror("Reading from interface");
+      close(net_fd);
+      exit(1);
+    }
+        if(kread < 0) {
+      perror("Reading from interface");
+      close(net_fd);
+      exit(1);
+    }
+
+    /* Do whatever with the data */
+    printf("Read %d bytes from device %s\n", kread, if_name);
+    for (int i = 0; i < kread; i++) {
+      if ((i!=0) && (i%16==0)) {
+        printf("\n");
+      }
+      printf("%02X ", buffer[i]);
+    }
+    printf("\n");
+    
+
+    // kread = read(net_fd,buffer,sizeof(buffer));
+    // if(kread < 0) {
+    //   perror("Reading from interface");
+    //   close(net_fd);
+    //   exit(1);
+    // }
+    //     if(kread < 0) {
+    //   perror("Reading from interface");
+    //   close(net_fd);
+    //   exit(1);
+    // }
+
+    // /* Do whatever with the data */
+    // printf("Read %d bytes from device %s\n", kread, if_name);
+    // for (int i = 0; i < kread; i++) {
+    //   if ((i!=0) && (i%16==0)) {
+    //     printf("\n");
+    //   }
+    //   printf("%02X ", buffer[i]);
+    // }
+    // printf("\n");
+    
+  while(1) {
+    /* Note that "buffer" should be at least the MTU size of the interface, eg 1500 bytes */
+
+
+    /* Do whatever with the data */
+
+    write(net_fd, buffer, sizeof(buffer));
+    write(net_fd, buffer, sizeof(buffer));
+    write(net_fd, buffer, sizeof(buffer));
+    write(net_fd, buffer, sizeof(buffer));
+    write(net_fd, buffer, sizeof(buffer));
+    write(net_fd, buffer, sizeof(buffer));
+
+    sleep(4);
+    // for(int i = 0; i < 0x200; i++){
+    //   int fd = write(net_fd, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 2000);
+    //   if(fd <= 0) printf("fucking sibal\n");
+    //   else printf("ezpzhaha\n");
+    // }
+
+  }
+  
+  return(0);
+}
